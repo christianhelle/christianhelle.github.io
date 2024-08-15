a29cd1168a71ce72ca7163767f84c6700c67e693
Christian Helle <christian.helle@outlook.com>
2024-08-15 08:49:44 +0200

    chore: Update start script to use _config_dev.yml for local development

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
-rm _config.yml
-cp _config_dev.yml _config.yml
-
 rm -rf _site
+cp -f _config_dev.yml _config.yml
 bundle install && bundle exec jekyll serve --incremental
-
-rm _config.yml
-cp _config_prod.yml _config.yml
 
